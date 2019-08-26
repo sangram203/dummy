f79b10e1680e1671c7208b73fe369d4ef6c71a7e
sangram203 <sangram.hati@gmail.com>
2019-08-26 09:51:04 +0530

    addas

---
+++ functional-combination-lock/functional-combination-lock/README.markdown
@@ -0,0 +1,4 @@
+# Functional Combination Lock
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/_Blueeby/pen/WqoGBx](https://codepen.io/_Blueeby/pen/WqoGBx).
+
+ Functioning combination lock PROOF OF CONCEPT with CSS and jQuery. Could be used for games, passwords, lock screens, etc. The combination is 8-7-5-9-0.
---
+++ v1.1/index.js
@@ -0,0 +1,87 @@
+var carousel = document.querySelector('.carousel');
+var cells = carousel.querySelectorAll('.carousel__cell');
+var cellCount; // cellCount set from cells-range input value
+var selectedIndex = 0;
+var cellWidth = carousel.offsetWidth;
+var cellHeight = carousel.offsetHeight;
+var isHorizontal = true;
+var rotateFn = isHorizontal ? 'rotateX' : 'rotateY';
+var radius, theta;
+// console.log( cellWidth, cellHeight );
+
+function rotateCarousel() {
+  var angle = theta * selectedIndex * -1;
+  carousel.style.transform = 'translateZ(' + -radius + 'px) ' + 
+    rotateFn + '(' + angle + 'deg)';
+}
+
+var prevButton = document.querySelector('.previous-button');
+prevButton.addEventListener( 'click', function() {
+  selectedIndex--;
+  rotateCarousel();
+});
+
+var nextButton = document.querySelector('.next-button');
+nextButton.addEventListener( 'click', function() {
+  selectedIndex++;
+  rotateCarousel();
+});
+
+var lastScrollTop = 0;
+var mainWrapper = document.querySelector('.mainWrapper');
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+mainWrapper.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+   if (st > lastScrollTop){
+    selectedIndex++;
+    rotateCarousel();
+   } else {
+    selectedIndex--;
+    rotateCarousel();
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+var cellsRange = document.querySelector('.cells-range');
+cellsRange.addEventListener( 'change', changeCarousel );
+cellsRange.addEventListener( 'input', changeCarousel );
+
+
+
+function changeCarousel() {
+  cellCount = cellsRange.value;
+  theta = 360 / cellCount;
+  var cellSize = isHorizontal ? cellHeight : cellWidth;
+  radius = Math.round( ( cellSize / 2) / Math.tan( Math.PI / cellCount ) );
+  for ( var i=0; i < cells.length; i++ ) {
+    var cell = cells[i];
+    if ( i < cellCount ) {
+      // visible cell
+      cell.style.opacity = 1;
+      var cellAngle = theta * i;
+      cell.style.transform = rotateFn + '(' + cellAngle + 'deg) translateZ(' + radius + 'px)';
+    } else {
+      // hidden cell
+      cell.style.opacity = 0;
+      cell.style.transform = 'none';
+    }
+  }
+
+  rotateCarousel();
+}
+
+var orientationRadios = document.querySelectorAll('input[name="orientation"]');
+( function() {
+  for ( var i=0; i < orientationRadios.length; i++ ) {
+    var radio = orientationRadios[i];
+    radio.addEventListener( 'change', onOrientationChange );
+  }
+})();
+
+function onOrientationChange() {
+  isHorizontal = true
+  rotateFn = isHorizontal ? 'rotateX' : 'rotateY';
+  changeCarousel();isHorizontal
+}
+
+// set initials
+onOrientationChange();
